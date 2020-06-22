8971d0f65a101a27b828b950df3f467e413d4594
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2020-06-22 10:25:30 -0400

    Initial Commit

---
+++ ForOQs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForOQs
+//
+//  Created by Zhaoyang Li on 6/22/20.
+//  Copyright © 2020 Zhaoyang Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
